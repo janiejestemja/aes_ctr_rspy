2d40ad703323c32f7e248bc9851909c9b49f9520
janiejestemja <janiejestemja@web.de>
2025-04-14 17:07:13 +0200

    edited usage.py

--- usage.py
+++ usage.py
@@ -1,19 +1,19 @@
 from aes_ctr_rspy import AesCtrSecret
 
-key = bytearray(b"2" * 32)
-nonce = bytearray(b"4" * 8)
+key = bytearray(("2" * 32).encode())
+nonce = bytearray(("4" * 8).encode())
 secret = AesCtrSecret(key, nonce)
 
 print("Key: ", key)
 print("Nonce: ", nonce)
 
-data = bytearray(b"This is a little longer test message than usual, to check if CTR is working as intended...")
+data = bytearray("This is a little longer test message than usual, to check if CTR is working as intended...".encode())
 print(data)
 
 ciphertext = bytearray(secret.encrypt(data))
 
-key = bytearray(b"2" * 32)
-nonce = bytearray(b"4" * 8)
+key = bytearray(("2" * 32).encode())
+nonce = bytearray(("4" * 8).encode())
 secret2 = AesCtrSecret(key, nonce)
 
 deciphered = secret2.encrypt(ciphertext)
