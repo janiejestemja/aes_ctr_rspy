8e7b0ba3232613ed713caab77580f32952260ec9
janiejestemja <janiejestemja@web.de>
2025-05-09 16:06:27 +0200

    added demo dir for usage.py and added kpa.py

--- demo/usage.py
+++ demo/usage.py
@@ -4,17 +4,15 @@ key = bytearray(("2" * 32).encode())
 nonce = bytearray(("4" * 8).encode())
 secret = AesCtrSecret(key, nonce)
 
-print("Key: ", key)
-print("Nonce: ", nonce)
-
 data = bytearray("This is a little longer test message than usual, to check if CTR is working as intended...".encode())
-print(data)
+print("Plaintext: ", data, "\n")
 
 ciphertext = bytearray(secret.encrypt(data))
+print("Ciphertext: ", ciphertext, "\n")
 
 key = bytearray(("2" * 32).encode())
 nonce = bytearray(("4" * 8).encode())
 secret2 = AesCtrSecret(key, nonce)
 
 deciphered = secret2.encrypt(ciphertext)
-print(deciphered)
+print("Deciphered text: ", deciphered)
